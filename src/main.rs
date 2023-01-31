f357f15835fd9e9f00a2f5d3100d186319eb82f3
Kiran Shila <me@kiranshila.com>
2023-01-31 11:09:27 -0800

    byte length

--- src/main.rs
+++ src/main.rs
@@ -62,8 +62,6 @@ impl Payload {
     #[must_use]
     pub fn from_bytes(bytes: &[u8]) -> Self {
         let mut payload = Payload::default();
-        println!("{}", bytes.len());
-        println!("{}", bytes[TIMESTAMP_SIZE..].len());
         for (i, word) in bytes[TIMESTAMP_SIZE..].chunks_exact(WORD_SIZE).enumerate() {
             // Each word contains two frequencies for each polarization
             // [A1 B1 A2 B2]
@@ -90,7 +88,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mut counts = vec![0u64; CAP_PACKS];
         let mut packets = 0usize;
         while packets < CAP_PACKS {
-            let buf = vec![0; 16_384];
+            let buf = vec![0; PAYLOAD_SIZE];
             let (result, buf) = sock.recv_from(buf).await;
             if let Ok((n, _)) = result {
                 if n == PAYLOAD_SIZE {
